2230a5d09ac4bf6ade5c02a49a1955ba38e27a8b
Ihar Berasneu <igor.simatic@gmail.com>
2021-04-09 14:13:49 +0300

    refactor: change User from functional component to class component:lesson 53

--- way/src/components/Users/Users.jsx
+++ way/src/components/Users/Users.jsx
@@ -1,43 +1,50 @@
-
+import React from 'react'
 import * as axios from 'axios';
-let Users = (props) => {
-  if (props.users.length === 0) {
+
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props)
 
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      props.setUsers(response.data.items);
+      this.props.setUsers(response.data.items);
     });
 
-
-  };
-
-
-  return (
-    <div>
-      {
-        props.users.map((user) => <div className='user-content' key={user.id}>
-          <div className='user-content__logo'>
-            <img src={user.photos.small != null ? user.photos.small : '/photoFriends/MyPhoto.png'} alt="" />
-            {user.followed
-              ? <button onClick={() => {
-                props.unfollow(user.id)
-              }}>Unfollow</button>
-              : <button onClick={() => {
-                props.follow(user.id)
-              }}>Follow</button>}
-          </div>
-          <div className='user-content__info'>
-            <p>{user.name}</p>
-            <p>{user.status}</p>
-            <p>{/* user.location.city */}</p>
-            <p>{/* user.location.country */}</p>
-          </div>
-        </div>)
-      }
+  }
 
 
 
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        {
+          this.props.users.map((user) => <div className='user-content' key={user.id}>
+            <div className='user-content__logo'>
+              <img src={user.photos.small != null ? user.photos.small : '/photoFriends/MyPhoto.png'} alt="" />
+              {user.followed
+                ? <button onClick={() => {
+                  this.props.unfollow(user.id)
+                }}>Unfollow</button>
+                : <button onClick={() => {
+                  this.props.follow(user.id)
+                }}>Follow</button>}
+            </div>
+            <div className='user-content__info'>
+              <p>{user.name}</p>
+              <p>{user.status}</p>
+              <p>{/* user.location.city */}</p>
+              <p>{/* user.location.country */}</p>
+            </div>
+          </div>)
+        }
+
+
+
+      </div>
+    )
+  }
+
+
 }
 
 export default Users;
