7674c8c30734b3b370c9834f48aeb07efe058672
Ihar Berasneu <igor.simatic@gmail.com>
2021-04-16 22:09:16 +0300

    feat:add redux-form part1:lesson75

--- way/src/components/Login/Login.jsx
+++ way/src/components/Login/Login.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const Login = () => {
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div><Field type="text" name="login" id="" placeholder={'Login'} component={'input'} /></div>
+            <div><Field type="text" name="password" id="" placeholder={'Password'} component={'input'} /></div>
+            <div><Field type="checkbox" name="rememberMe" id="" component={'input'} />Remember me</div>
+            <div><button>Login</button></div>
+        </form>
+
+    )
+}
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
     return (
         <div>
-            Login
+            <h2>Login</h2>
+            <LoginReduxForm onSubmit={onSubmit} />
         </div>
     )
 }
